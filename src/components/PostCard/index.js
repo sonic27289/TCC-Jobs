e9a074dae4aacca00246c53f5de1b191ceaa80db
sonic27289 <sonic27289@gmail.com>
2022-09-15 18:24:29 -0300

    Alterações

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,8 +1,83 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Typography from '@material-ui/core/Typography';
+import Avatar from "@material-ui/core/Avatar";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import MessageIcon from '@material-ui/icons/Message';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginBottom: theme.spacing(2)
+    },
+    subheader: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    caption: {
+        marginRight: theme.spacing(1)
+    },
+    message: {
+        height: 'auto',
+        marginBottom: theme.spacing(2),
+        padding: '0 24px'
+    },
+    image: {
+        height: 300,
+        width: '100%',
+        maxWidth: '100%'
+    },
+    content: {
+        padding: 0
+    } 
+}));
 
 function PostCard({ post }){
+    const classes = useStyles();
+
     return(
-        <h1>{post.description}</h1>
+        <Card className={classes.root}>
+            <CardHeader
+            avatar = {<Avatar src={post.author.avatar}></Avatar>}
+            title = {<Typography variant="h6">{post.title}</Typography>}
+            subheader = {
+                <div className={classes.subheader}>
+                    <Typography variant="caption" className={classes.caption}>
+                        {'Avaliado por'}
+                    </Typography>
+
+                    <Typography variant="subtitle2" className={classes.caption}>
+                    {post.author.name}
+                    </Typography>
+
+                    <Typography variant="caption" className={classes.caption}>
+                    {post.date}
+                    </Typography>
+                </div>
+            }
+            >
+            </CardHeader>
+            <CardContent className={classes.content}>
+                <Typography 
+                    variant="body1" 
+                    className={classes.message}
+                    >
+                        {post.hashtags}
+                </Typography>
+                <CardActionArea>
+                    <img src={post.image} className={classes.image}></img>
+                </CardActionArea>
+            </CardContent>
+            <CardActions disableSpacing>
+
+            </CardActions>
+        </Card>
     ) 
 }
 
--- src/paginas/Home/components/Feed.js
+++ src/paginas/Home/components/Feed.js
@@ -10,11 +10,43 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const posts = [
-    { id: 1, description: 'teste 1'},
-    { id: 2, description: 'teste 2'}
+    // { id: 1, description: 'teste 1'},
+    // { id: 2, description: 'teste 2'},
+    {
+        id: 1,
+        author: {
+            id: 1,
+            name: 'Gustavo Barbosa',
+            username: 'sonic27289',
+            avatar: '/images/avatars/iconeperfil.jpg'
+
+        },
+        title: "Comparativo entre jQuery e React.js - Performance",
+        date: "Setembro 15, 2022",
+        description: "Teste para o Trabalho de Conclusão de Curso, JOBS",
+        hashtags: "#frameworks, #javascript, #jquery, #reactjs",
+        image: "/images/posts/imagempost1.jpg"
+    },
+    {
+        id: 2,
+        author: {
+            id: 1,
+            name: 'Gustavo Barbosa',
+            username: 'sonic27289',
+            avatar: '/images/avatars/iconeperfil.jpg'
+
+        },
+        title: "Comparativo entre jQuery e React.js - Performance",
+        date: "Setembro 15, 2022",
+        description: "Teste para o Trabalho de Conclusão de Curso, JOBS",
+        hashtags: "#frameworks, #javascript, #jquery, #reactjs",
+        image: "/images/posts/imagempost1.jpg"
+    }
 ];
 
 function Feed(){
+    const classes = useStyles();
+
     return (
         <div>
             {
