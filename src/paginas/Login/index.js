8ba9cc04e83020daf63dd4269ced42b5a6f6f789
sonic27289 <sonic27289@gmail.com>
2022-09-22 15:48:23 -0300

    Desenvolvimento Login
    Desenvolvimento da Página de Login, uso dos princípios básicos do HTML e CSS, iniciando o desenvolvimento com o Material UI

--- src/paginas/Login/index.js
+++ src/paginas/Login/index.js
@@ -1,8 +1,68 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        // display: 'flex',
+        // flexDirection: 'row',
+        // height: '100vh',
+
+    },
+    // left: {
+    //     background: 'blue',
+    //     flexGrow: 0,
+    //     flexBasis: '58%',
+
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     justifyContent: 'center',
+    //     alignItems: 'center'
+    // },
+    // right: {
+    //     background: 'yellow',
+    //     flexGrow: 0,
+    //     flexBasis: '42%'
+    // },
+    // form: {
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     margin: '64px 32px',
+    //     alignItems: 'center',
+
+    // }
+}));
 
 function Login(){
+    const classes = useStyles();
+
     return (
-        <h1>Login Component</h1>
+       <Grid>
+        
+       </Grid>
+        // /* Flex Container */
+        // <div className={classes.root}>
+        //     {/* Flex Item / Container */}
+        //     <div className={classes.left}>
+        //         <Typography style={{color: '#fff', fontSize: 35, lineHeight: '45px'}}>
+        //             <strong>Teste aaaaaaaaaaaaaaaaa Teste aaaaaaaaaaaaaaaaaa</strong>
+        //         </Typography>
+        //         <Typography variant="body2" style={{color: 'rgb(255,255,255, 0.7)', marginTop: 30, fontSize: 15, lineHeight: '30px'}}>
+        //             Compartilhe o CONHECIMETO ET BILUUU MEU AMIIGo
+        //         </Typography>
+        //     </div>
+        //     {/* Flex Item  */}
+        //     <div className={classes.right}>
+        //         <form className={classes.form}>
+        //             <h2>Acesso</h2>
+        //             <input type="text"></input>
+        //             <input type="text"></input>
+        //         </form>
+        //     </div>
+        // </div>
     )
 }
 
